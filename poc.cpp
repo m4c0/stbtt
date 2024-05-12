21691ecf691fed0b0be5b38b93611c6c44ef9166
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-12 10:50:57 +0100

    poc: pulling ASCII-art example from header

--- poc.cpp
+++ poc.cpp
@@ -1,4 +1,25 @@
 #pragma leco tool
 import stbtt;
 
-int main() {}
+#include <stdio.h>
+
+unsigned char ttf_buffer[1 << 25];
+
+int main(int argc, char **argv) {
+  stbtt_fontinfo font;
+  unsigned char *bitmap;
+  int w, h, i, j, c = 'a', s = 20;
+
+  fread(ttf_buffer, 1, 1 << 25, fopen("VictorMono-Regular.otf", "rb"));
+
+  stbtt_InitFont(&font, ttf_buffer, stbtt_GetFontOffsetForIndex(ttf_buffer, 0));
+  bitmap = stbtt_GetCodepointBitmap(
+      &font, 0, stbtt_ScaleForPixelHeight(&font, s), c, &w, &h, 0, 0);
+
+  for (j = 0; j < h; ++j) {
+    for (i = 0; i < w; ++i)
+      putchar(" .:ioVM@"[bitmap[j * w + i] >> 5]);
+    putchar('\n');
+  }
+  return 0;
+}
